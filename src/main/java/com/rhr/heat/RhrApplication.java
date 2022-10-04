0e6561af57f7f3f06eb4307baa8248eccb9410bf
mahmoud-eltahawy <gemmy9300@gmail.com>
2022-10-05 00:19:33 +0200

    first commit

---
+++ src/main/java/com/rhr/heat/RhrApplication.java
@@ -0,0 +1,13 @@
+package com.rhr.heat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rhr/heat/RhrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhr.heat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
