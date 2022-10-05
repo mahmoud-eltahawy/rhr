7c0b31096fbdf3e878271c41ca6a913b6b37f21c
mahmoud-eltahawy <gemmy9300@gmail.com>
2022-10-05 05:44:48 +0200

    test zero completed

--- src/main/java/com/rhr/heat/service/Tester.java
+++ src/main/java/com/rhr/heat/service/Tester.java
@@ -40,55 +40,118 @@ public class Tester {
 		ProblemDetail pd2 = new ProblemDetail(null, Problem.p2, 1.5);
 		ProblemDetail pd3 = new ProblemDetail(null, Problem.p3, 0.5);
 		
-		Employee emp = new Employee(null,"mahmoud","gamal","mohammed",EmployeePosition.worker);
-		
-		employeeRepo.save(emp);
-		
 		problemDetailsRepo.save(pd1);
 		problemDetailsRepo.save(pd2);
 		problemDetailsRepo.save(pd3);
 		
-		KilensShiftStatus kilens = new KilensShiftStatus();
-		kilens.setKiln1(List.of(pd1,pd2,pd3));
-		kilens.setKiln2(List.of(pd2,pd3));
-		kilens.setKiln3(List.of(pd1,pd3));
-		kilens.setKiln4(List.of(pd1,pd2));
-		kilens.setKiln5(List.of(pd1));
+		Employee emp1 = new Employee(null,"taha","mohammed","ismaail",EmployeePosition.worker);
+		Employee emp2 = new Employee(null,"mohammed","gomaa","ali",EmployeePosition.worker);
+		Employee emp3 = new Employee(null,"mahmoud","gamal","mohammed",EmployeePosition.worker);
 		
-		kilensShiftStatusRepo.save(kilens);
+		employeeRepo.save(emp1);
+		employeeRepo.save(emp2);
+		employeeRepo.save(emp3);
 		
-		DrayersShiftStatus drayers = new DrayersShiftStatus();
-		drayers.setDrayer1(List.of(pd1,pd2,pd3));
-		drayers.setDrayer2(List.of(pd2,pd3));
-		drayers.setDrayer3(List.of(pd1,pd3));
-		drayers.setDrayer4(List.of(pd1,pd2));
-		drayers.setDrayer5(List.of(pd3));
-		drayers.setDrayer6(List.of(pd1));
-		drayers.setDrayer7(List.of(pd2));
+		KilensShiftStatus kilens1 = new KilensShiftStatus();
+		kilens1.setKiln1(List.of(pd1));
+		kilens1.setKiln2(List.of(pd1));
+		kilens1.setKiln3(List.of(pd1));
+		kilens1.setKiln4(List.of(pd1));
+		kilens1.setKiln5(List.of(pd1));
 		
-		drayersShiftStatusRepo.save(drayers);
+		KilensShiftStatus kilens2 = new KilensShiftStatus();
+		kilens2.setKiln1(List.of(pd2));
+		kilens2.setKiln2(List.of(pd2));
+		kilens2.setKiln3(List.of(pd2));
+		kilens2.setKiln4(List.of(pd2));
+		kilens2.setKiln5(List.of(pd2));
 		
-		AtmsShiftStatus atms = new AtmsShiftStatus();
-		atms.setAtm1(List.of(pd2,pd3));
-		atms.setAtm1(List.of(pd1));
+		KilensShiftStatus kilens3 = new KilensShiftStatus();
+		kilens3.setKiln1(List.of(pd1,pd2,pd3));
+		kilens3.setKiln2(List.of(pd2,pd3));
+		kilens3.setKiln3(List.of(pd1,pd3));
+		kilens3.setKiln4(List.of(pd1,pd2));
+		kilens3.setKiln5(List.of(pd1));
 		
-		atmsShiftStatusRepo.save(atms);
+		kilensShiftStatusRepo.save(kilens1);
+		kilensShiftStatusRepo.save(kilens2);
+		kilensShiftStatusRepo.save(kilens3);
+		
+		DrayersShiftStatus drayers1 = new DrayersShiftStatus();
+		drayers1.setDrayer1(List.of(pd2));
+		drayers1.setDrayer2(List.of(pd2));
+		drayers1.setDrayer3(List.of(pd2));
+		drayers1.setDrayer4(List.of(pd2));
+		drayers1.setDrayer5(List.of(pd2));
+		drayers1.setDrayer6(List.of(pd2));
+		drayers1.setDrayer7(List.of(pd2));
+		
+		DrayersShiftStatus drayers2 = new DrayersShiftStatus();
+		drayers2.setDrayer1(List.of(pd3));
+		drayers2.setDrayer2(List.of(pd3));
+		drayers2.setDrayer3(List.of(pd3));
+		drayers2.setDrayer4(List.of(pd3));
+		drayers2.setDrayer5(List.of(pd3));
+		drayers2.setDrayer6(List.of(pd3));
+		drayers2.setDrayer7(List.of(pd3));
+		
+		DrayersShiftStatus drayers3 = new DrayersShiftStatus();
+		drayers3.setDrayer1(List.of(pd1,pd2,pd3));
+		drayers3.setDrayer2(List.of(pd2,pd3));
+		drayers3.setDrayer3(List.of(pd1,pd3));
+		drayers3.setDrayer4(List.of(pd1,pd2));
+		drayers3.setDrayer5(List.of(pd3));
+		drayers3.setDrayer6(List.of(pd1));
+		drayers3.setDrayer7(List.of(pd2));
+		
+		drayersShiftStatusRepo.save(drayers1);
+		drayersShiftStatusRepo.save(drayers2);
+		drayersShiftStatusRepo.save(drayers3);
+		
+		AtmsShiftStatus atms1 = new AtmsShiftStatus();
+		atms1.setAtm1(List.of(pd3));
+		atms1.setAtm1(List.of(pd3));
+		
+		AtmsShiftStatus atms2 = new AtmsShiftStatus();
+		atms2.setAtm1(List.of(pd2));
+		atms2.setAtm1(List.of(pd3));
+		
+		AtmsShiftStatus atms3 = new AtmsShiftStatus();
+		atms3.setAtm1(List.of(pd2,pd3));
+		atms3.setAtm1(List.of(pd1));
+		
+		atmsShiftStatusRepo.save(atms1);
+		atmsShiftStatusRepo.save(atms2);
+		atmsShiftStatusRepo.save(atms3);
+		
+		Shift shift1 = new Shift();
+		shift1.setKilens(kilens1);
+		shift1.setDrayers(drayers1);
+		shift1.setAtms(atms1);
+		shift1.setEmployees(List.of(emp1));
+		
+		Shift shift2 = new Shift();
+		shift2.setKilens(kilens3);
+		shift2.setDrayers(drayers3);
+		shift2.setAtms(atms3);
+		shift2.setEmployees(List.of(emp2));
 		
 		Shift shift3 = new Shift();
-		shift3.setKilens(kilens);
-		shift3.setDrayers(drayers);
-		shift3.setAtms(atms);
-		shift3.setEmployees(List.of(emp));
+		shift3.setKilens(kilens3);
+		shift3.setDrayers(drayers3);
+		shift3.setAtms(atms3);
+		shift3.setEmployees(List.of(emp3));
 		
+		shiftRepo.save(shift1);
+		shiftRepo.save(shift2);
 		shiftRepo.save(shift3);
 		
 		WorkDay workDay = new WorkDay();
-		workDay.setShift1(null);
-		workDay.setShift2(null);
+		workDay.setShift1(shift1);
+		workDay.setShift2(shift2);
 		workDay.setShift3(shift3);
 		workDay.setDate(new Date());
 		
 		workdayRepo.save(workDay);
-		
 	}
 }
