083858b6cf1f1197563e7dd46c8596d7e9306b75
mahmoud-eltahawy <gemmy9300@gmail.com>
2022-10-08 11:12:08 +0200

    java code dependent database scheme

--- schema.sql
+++ schema.sql
@@ -3,60 +3,53 @@ CREATE TABLE employee (
 	first_name   VARCHAR(40) NOT NULL,
 	middle_name  VARCHAR(40) NOT NULL,
 	last_name    VARCHAR(40) NOT NULL,
-    emp_position INTEGER     NOT NULL
+    emp_position varchar(20) NOT NULL
 );
 
 CREATE TABLE total_flow (
-    id         BIGSERIAL NOT NULL PRIMARY KEY,
-    atms_case  INTEGER   NOT NULL,
-    begin_time TIME      NOT NULL,
-    end_time   TIME      NOT NULL,
-    min_flow   INTEGER   NOT NULL,
-    max_flow   INTEGER   NOT NULL
+    id         BIGSERIAL   NOT NULL PRIMARY KEY,
+    atms_case  varchar(20) NOT NULL,
+    begin_time TIME        NOT NULL,
+    end_time   TIME        NOT NULL,
+    min_flow   INTEGER     NOT NULL,
+    max_flow   INTEGER     NOT NULL
 );
 
 CREATE TABLE problem_detail (
-    id         BIGSERIAL NOT NULL PRIMARY KEY,
-    problem    INTEGER   NOT NULL,
-    machine    INTEGER   NOT NULL,
-    begin_time TIME      NOT NULL,
-    end_time   TIME      NOT NULL
-);
-
-CREATE TABLE shift_id (
-    id          BIGSERIAL PRIMARY KEY,
-    shift_date  DATE      NOT NULL,
-    shift_order INTEGER   NOT NULL
+    id         BIGSERIAL   NOT NULL PRIMARY KEY,
+    problem    varchar(40) NOT NULL,
+    machine    varchar(20) NOT NULL,
+    begin_time TIME        NOT NULL,
+    end_time   TIME        NOT NULL
 );
 
 CREATE TABLE shift (
-    shift_id BIGINT  PRIMARY KEY,
-    max_temp INTEGER NOT NULL,
-    min_temp INTEGER NOT NULL,
-    notes    VARCHAR(255),
-    FOREIGN KEY(shift_id) REFERENCES shift_id(id) ON DELETE CASCADE
+    id varchar(20)  PRIMARY KEY,
+    max_temp INTEGER      NOT NULL,
+    min_temp INTEGER      NOT NULL,
+    notes    VARCHAR(255)
 );
 
 CREATE TABLE shift_problem (
-    shift_id   BIGINT NOT NULL,
-    problem_id BIGINT NOT NULL,
+    shift_id   varchar(20)   NOT NULL,
+    problem_id BIGINT        NOT NULL,
     PRIMARY KEY(shift_id,problem_id),
-    FOREIGN KEY(shift_id)    REFERENCES shift(shift_id)    ON DELETE CASCADE,
+    FOREIGN KEY(shift_id)    REFERENCES shift(id)          ON DELETE CASCADE,
     FOREIGN KEY(problem_id)  REFERENCES problem_detail(id) ON DELETE CASCADE
 );
 
 CREATE TABLE shift_total_flow (
-    shift_id BIGINT NOT NULL,
-    flow_id  BIGINT NOT NULL,
+    shift_id varchar(20)   NOT NULL,
+    flow_id  BIGINT        NOT NULL,
     PRIMARY  KEY(shift_id,flow_id),
-    FOREIGN  KEY(shift_id) REFERENCES shift(shift_id) ON DELETE CASCADE,
+    FOREIGN  KEY(shift_id) REFERENCES shift(id)       ON DELETE CASCADE,
     FOREIGN  KEY(flow_id)  REFERENCES total_flow(id)  ON DELETE CASCADE
 );
 
 CREATE TABLE shift_employee (
-    shift_id BIGINT NOT NULL,
-    emp_id   BIGINT NOT NULL,
+    shift_id varchar(20)   NOT NULL,
+    emp_id   BIGINT        NOT NULL,
     PRIMARY  KEY(shift_id,emp_id),
-    FOREIGN  KEY(shift_id) REFERENCES shift(shift_id) ON DELETE CASCADE,
-    FOREIGN  KEY(emp_id)  REFERENCES employee(id)    ON DELETE CASCADE
+    FOREIGN  KEY(shift_id) REFERENCES shift(id)    ON DELETE CASCADE,
+    FOREIGN  KEY(emp_id)   REFERENCES employee(id) ON DELETE CASCADE
 );
