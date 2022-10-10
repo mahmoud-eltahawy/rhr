1473f919e6a640da04f2d489aef7d60773b68ff4
mahmoud-eltahawy <gemmy9300@gmail.com>
2022-10-10 08:18:35 +0200

    id got

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE employee (
+CREATE TABLE IF NOT EXISTS employee (
     id           BIGSERIAL   NOT NULL PRIMARY KEY,
 	first_name   VARCHAR(40) NOT NULL,
 	middle_name  VARCHAR(40) NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE employee (
     emp_position VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE total_flow (
+CREATE TABLE IF NOT EXISTS total_flow (
     id           BIGSERIAL   NOT NULL PRIMARY KEY,
     atms_case    VARCHAR(20) NOT NULL,
     begin_time   TIME        NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE total_flow (
     max_flow     INTEGER     NOT NULL
 );
 
-CREATE TABLE problem_detail (
+CREATE TABLE IF NOT EXISTS problem_detail (
     id           BIGSERIAL   NOT NULL PRIMARY KEY,
     problem      VARCHAR(40) NOT NULL,
     machine      VARCHAR(20) NOT NULL,
@@ -23,14 +23,14 @@ CREATE TABLE problem_detail (
     end_time     TIME        NOT NULL
 );
 
-CREATE TABLE shift_id (
+CREATE TABLE IF NOT EXISTS shift_id (
     id          BIGSERIAL     PRIMARY KEY,
     shift_order VARCHAR(20)   NOT NULL,
     shift_date  DATE          NOT NULL,
     CONSTRAINT shift_identity UNIQUE(shift_order,shift_date)
 );
 
-CREATE TABLE shift (
+CREATE TABLE IF NOT EXISTS shift (
     shift_id BIGINT       PRIMARY KEY,
     max_temp INTEGER      NOT NULL,
     min_temp INTEGER      NOT NULL,
@@ -38,7 +38,7 @@ CREATE TABLE shift (
     FOREIGN KEY(shift_id) REFERENCES shift_id(id) ON DELETE CASCADE
 );
 
-CREATE TABLE shift_problem (
+CREATE TABLE IF NOT EXISTS shift_problem (
     shift_id   BIGINT        NOT NULL,
     problem_id BIGINT        NOT NULL,
     PRIMARY KEY(shift_id,problem_id),
@@ -46,7 +46,7 @@ CREATE TABLE shift_problem (
     FOREIGN KEY(problem_id)  REFERENCES problem_detail(id) ON DELETE CASCADE
 );
 
-CREATE TABLE shift_total_flow (
+CREATE TABLE IF NOT EXISTS shift_total_flow (
     shift_id BIGINT        NOT NULL,
     flow_id  BIGINT        NOT NULL,
     PRIMARY  KEY(shift_id,flow_id),
@@ -54,7 +54,7 @@ CREATE TABLE shift_total_flow (
     FOREIGN  KEY(flow_id)  REFERENCES total_flow(id)  ON DELETE CASCADE
 );
 
-CREATE TABLE shift_employee (
+CREATE TABLE IF NOT EXISTS shift_employee (
     shift_id BIGINT        NOT NULL,
     emp_id   BIGINT        NOT NULL,
     PRIMARY  KEY(shift_id,emp_id),
--- src/main/java/com/rhr/heat/dao/ProblemDetailsRepo.java
+++ src/main/java/com/rhr/heat/dao/ProblemDetailsRepo.java
@@ -1,9 +1,13 @@
 package com.rhr.heat.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.rhr.heat.model.ProblemDetail;
@@ -27,13 +31,23 @@ public class ProblemDetailsRepo {
 		problems.forEach(p -> save(p));
 	}
 
-	public void save(ProblemDetail pd) {
-		String sql = "INSERT INTO problem_detail(problem, machine,"
-				+ "begin_time, end_time) VALUES(?,?,?,?)";
-		jdbcTemplate.update(sql,
-				pd.getProblem().toString(),
-				pd.getMachine().toString(),
-				pd.getBeginTime(),
-				pd.getEndTime());
+	public Long save(ProblemDetail pd) {
+		KeyHolder key = new GeneratedKeyHolder();
+		jdbcTemplate.update(connection ->{
+			PreparedStatement ps = connection
+					.prepareStatement("INSERT INTO problem_detail(problem, machine,"
+							+ "begin_time, end_time) VALUES(?,?,?,?)", 
+							Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, pd.getProblem().toString());
+			ps.setString(2, pd.getMachine().toString());
+			ps.setTime(3, pd.getBeginTime());
+			ps.setTime(4, pd.getEndTime());
+			return ps;
+		},key);
+	 if (key.getKeys().size() > 1) {
+			return (Long)key.getKeys().get("id");
+		} else {
+			return key.getKey().longValue();
+		}
 	}
 }
