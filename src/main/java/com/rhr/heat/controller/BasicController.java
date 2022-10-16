5faa2b674a246bba854f54c85745376632c51cc3
mahmoud-eltahawy <gemmy9300@gmail.com>
2022-10-16 03:43:38 +0200

    a+

--- src/main/java/com/rhr/heat/controller/BasicController.java
+++ src/main/java/com/rhr/heat/controller/BasicController.java
@@ -1,12 +1,17 @@
 package com.rhr.heat.controller;
 
 
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.TreeMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.rhr.heat.model.Day;
 import com.rhr.heat.service.ShiftService;
 
 import lombok.RequiredArgsConstructor;
@@ -18,8 +23,18 @@ public class BasicController {
 
 	@GetMapping("/")
 	public String index(Model model) {
+		TreeMap<Date, Day> week = service.pickLastWeeks(1);
 		model.addAttribute("title","Welcom to index page");
-		model.addAttribute("week",service.pickLastWeeks(1));
+		model.addAttribute("week", week);
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(week.firstKey());
+		model.addAttribute("fy",cal.get(Calendar.YEAR));
+		model.addAttribute("fm",cal.get(Calendar.MONTH));
+		model.addAttribute("fd",cal.get(Calendar.DAY_OF_MONTH));
+		cal.setTime(week.lastKey());
+		model.addAttribute("ly",cal.get(Calendar.YEAR));
+		model.addAttribute("lm",cal.get(Calendar.MONTH));
+		model.addAttribute("ld",cal.get(Calendar.DAY_OF_MONTH));
 		return "index";
 	}
 	
--- src/main/java/com/rhr/heat/service/ShowingService.java
+++ src/main/java/com/rhr/heat/service/ShowingService.java
@@ -1,10 +1,13 @@
 package com.rhr.heat.service;
 
+import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.TreeMap;
 
 import org.springframework.stereotype.Service;
 
@@ -12,6 +15,7 @@ import com.rhr.heat.dao.ShiftRepo;
 import com.rhr.heat.entity.ProblemDetail;
 import com.rhr.heat.entity.Shift;
 import com.rhr.heat.enums.Machine;
+import com.rhr.heat.model.Day;
 
 import lombok.RequiredArgsConstructor;
 
@@ -40,5 +44,27 @@ public class ShowingService {
 		}
 		
 		return mp;
+	}
+	
+	public TreeMap<Date ,Day> pickWeekBefore(Integer year,Integer month,Integer day){
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.YEAR, year);
+		cal.set(Calendar.MONTH, month);
+		cal.set(Calendar.DAY_OF_MONTH, day);
+		Date newer = new Date(cal.getTime().getTime());
+		cal.add(Calendar.DAY_OF_MONTH, -6);
+		Date older = new Date(cal.getTime().getTime());
+		return Day.getDays(shiftRepo.findBetween(older, newer, false));
+	}
+	
+	public TreeMap<Date ,Day> pickWeekAfter(Integer year,Integer month,Integer day){
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.YEAR, year);
+		cal.set(Calendar.MONTH, month);
+		cal.set(Calendar.DAY_OF_MONTH, day);
+		Date older = new Date(cal.getTime().getTime());
+		cal.add(Calendar.DAY_OF_MONTH, 6);
+		Date newer = new Date(cal.getTime().getTime());
+		return Day.getDays(shiftRepo.findBetween(older, newer, false));
 	}
 }
