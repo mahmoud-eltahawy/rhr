b879ad7733d3b8b5fba0cda910aeb1c36704e7a8
mahmoud-eltahawy <gemmy9300@gmail.com>
2022-10-27 07:40:27 +0200

    r

--- src/main/java/com/rhr/heat/service/ReportService.java
+++ src/main/java/com/rhr/heat/service/ReportService.java
@@ -1,7 +1,6 @@
 package com.rhr.heat.service;
 
 import java.sql.Date;
-import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
@@ -14,32 +13,18 @@ import com.rhr.heat.enums.ShiftOrder;
 @Service
 public class ReportService {
 	
-	public ShiftId thisShift(Calendar cal) {
-		cal = check(cal);
-		Timestamp now = new Timestamp(cal.getTimeInMillis());
+	public ShiftId thisShift() {
+		Timestamp now = new Timestamp(check(Calendar.getInstance()).getTimeInMillis());
 
-		if(now.after(BeginOrEnd(ShiftOrder.THIRD, false)) &
+			   if(now.after(BeginOrEnd(ShiftOrder.THIRD, false)) &
 				now.before(BeginOrEnd(ShiftOrder.SECOND, true))) {
-			return new ShiftId(null,
-					new Date(cal.getTimeInMillis()),
-					ShiftOrder.FIRST);
+			return new ShiftId(null,new Date(now.getTime()), ShiftOrder.FIRST);
 		} else if(now.after(BeginOrEnd(ShiftOrder.FIRST, false)) &
 				now.before(BeginOrEnd(ShiftOrder.THIRD, true))) {
-			Date date = null;
-			if(cal.get(Calendar.AM_PM) == Calendar.AM &
-					cal.get(Calendar.HOUR) == 0 &
-					cal.get(Calendar.MINUTE) < 15 &
-					cal.get(Calendar.SECOND) < 60 &
-					cal.get(Calendar.MILLISECOND) < 1000) {
-				date = new Date(cal.getTimeInMillis() - TimeUnit.DAYS.toMillis(1));
-			} else {
-				date = new Date(cal.getTimeInMillis());
-			}
-			return new ShiftId(null, date, ShiftOrder.SECOND);
+			return new ShiftId(null, new Date(now.getTime()), ShiftOrder.SECOND);
 		} else if(now.before(BeginOrEnd(ShiftOrder.FIRST, true))) {
 			return new ShiftId(null,
-					new Date(cal.getTimeInMillis() - TimeUnit.DAYS.toMillis(1)),
-					ShiftOrder.THIRD);
+					new Date(now.getTime() - TimeUnit.DAYS.toMillis(1)),ShiftOrder.THIRD);
 		} else {
 			return null;
 		}
