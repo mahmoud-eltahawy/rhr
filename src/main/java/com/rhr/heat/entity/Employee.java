d678278c10706973749e4588d2a9aa353ddbed12
mahmoud-eltahawy <gemmy9300@gmail.com>
2022-10-28 13:59:26 +0200

    rf

--- src/main/java/com/rhr/heat/entity/Employee.java
+++ src/main/java/com/rhr/heat/entity/Employee.java
@@ -1,6 +1,7 @@
 package com.rhr.heat.entity;
 
 
+import java.util.Objects;
 import java.util.UUID;
 
 import com.rhr.heat.enums.EmployeePosition;
@@ -20,4 +21,19 @@ public class Employee {
 	private EmployeePosition position;
 	private String username;
 	private String password;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return Objects.equals(username, other.username);
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(username);
+	}
 }
