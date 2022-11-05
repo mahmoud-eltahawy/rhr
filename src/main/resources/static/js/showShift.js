baee21c78c9a2c1ce312f07ef59d0b1627d83602
mahmoud-eltahawy <gemmy9300@gmail.com>
2022-11-05 03:58:34 +0200

    bug fixed

--- src/main/resources/static/js/showShift.js
+++ src/main/resources/static/js/showShift.js
@@ -1,8 +1,17 @@
 function toggle(id) {
-  var x = document.getElementById(id);
+  const x = document.getElementById(id);
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
+}
+
+const allButtons = document.getElementsByTagName("button")
+
+for(let i = 0; i< allButtons.length ; i++){
+  const button = allButtons[i]
+  button.addEventListener("click", function(){
+    toggle(button.getAttribute("id").slice(0,-4))
+  })
 }
