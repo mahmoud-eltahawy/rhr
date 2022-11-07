5b0ef9ac6a7527c969c9af6625a2ca912110e217
mahmoud-eltahawy <gemmy9300@gmail.com>
2022-11-07 06:12:34 +0200

    dynamic searchButtons page

--- src/main/resources/static/js/searchButtons.js
+++ src/main/resources/static/js/searchButtons.js
@@ -1,8 +1,12 @@
-function toggle(id) {
-  var x = document.getElementById(id);
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+
+function addToAction(id){
+  document.querySelector("#action-place").innerHTML = document.getElementById(id).innerHTML;
+}
+
+const allButtons = document.getElementsByTagName("button")
+
+for(let i = 0; i < allButtons.length ; i++){
+  allButtons[i].addEventListener("click", function(){
+    addToAction(allButtons[i].getAttribute("id").slice(0,-4))
+  })
 }
