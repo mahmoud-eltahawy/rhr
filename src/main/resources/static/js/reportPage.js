e73eefd9d88d8f25720fcb751eaceab0eed34a1e
mahmoud-eltahawy <gemmy9300@gmail.com>
2022-11-10 05:53:00 +0200

    cc

--- src/main/resources/static/js/reportPage.js
+++ src/main/resources/static/js/reportPage.js
@@ -1,5 +1,6 @@
+const jsonMap = new Map(Object.entries(JSON.parse(document.querySelector("#catsContainer").innerText)))
+
 function addAddingButtons(){
-  const jsonMap = new Map(Object.entries(JSON.parse(document.querySelector("#catsContainer").innerText)))
   const mainField = document.querySelector("#add-problem-field")
 
   for (let catName of jsonMap.keys()) {
@@ -7,4 +8,19 @@ function addAddingButtons(){
   }
 }
 
+function addingButtons(){
+  for (let catName of jsonMap.keys()) {
+    document.querySelector("#"+"add-"+catName+"-btn").addEventListener("click",function(){
+      document.querySelector("#add-category-field").innerHTML = (function(){
+        let buttons = ""
+        for (let n of jsonMap.get(catName)){
+          buttons +=`<button class="cat-button" id="add-${catName}-${n}-btn">${catName}-${n}</button>`
+        }
+        return buttons;
+      })();
+    })
+  }
+}
+
 addAddingButtons()
+addingButtons()
