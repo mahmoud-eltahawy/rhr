90abaf106488e4c285c433d2b33b8b0e2a1f8a7a
mahmoud-eltahawy <gemmy9300@gmail.com>
2022-11-25 05:11:12 +0200

    reportPage null safe

--- ts/reportPage.ts
+++ ts/reportPage.ts
@@ -1,5 +1,5 @@
-const jsonMap : Map<string,number[]> = new Map(Object
-  .entries(JSON.parse(document.getElementById("catsContainer")!.innerText)))
+const promiseMap :Promise<Map<string,number[]>> =Promise.resolve(new Map(Object
+  .entries(JSON.parse(document.getElementById("catsContainer")!.innerText))))
 const names : string[] = JSON.parse(document.getElementById("namesContainer")!.innerText)
 
 function replaceForm(machine : string ,number: number ,fieldId: string){
@@ -37,30 +37,33 @@ function replaceForm(machine : string ,number: number ,fieldId: string){
   }
 }
 
-function replaceButtons(catName: string ,fieldId: string){
-  let arr : number[] | undefined = jsonMap.get(catName)
-  if(arr){
-    if(arr.length === 1){
-        replaceForm(catName,arr[0],fieldId)
-      } else if(arr.length > 1) {
-        const fieldDiv = document.getElementById(fieldId)
-        if(fieldDiv){
-          fieldDiv.innerHTML = (function(){
-          let buttons = ""
-          for (let n of arr){
-            buttons +=`<button class="cat-button"
-                          onclick="replaceForm('${catName}','${n}','${fieldId}')">
-                          ${catName} ${n}
-                        </button>`
+async function replaceButtons(catName: string ,fieldId: string){
+  try{
+    const jsonMap = await promiseMap
+    const arr : number[] | undefined = jsonMap.get(catName)
+    if(arr){
+      if(arr.length === 1){
+          replaceForm(catName,arr[0],fieldId)
+        } else if(arr.length > 1) {
+          const fieldDiv = document.getElementById(fieldId)
+          if(fieldDiv){
+            fieldDiv.innerHTML = (function(){
+            let buttons = ""
+            for (let n of arr){
+              buttons +=`<button class="cat-button"
+                            onclick="replaceForm('${catName}','${n}','${fieldId}')">
+                            ${catName} ${n}
+                          </button>`
+            }
+            return buttons
+            })()
           }
-          return buttons
-          })()
-        }
+      }
     }
+  } catch(err){
+    console.log(err)
   }
 }
-
-
 
 function toggle(id :string) {
   const x = document.getElementById(id)
@@ -73,25 +76,37 @@ function toggle(id :string) {
   }
 }
 
-function getCategoriesContainers() : Map<string,{vsize: number,catnum: {cat: string, num: number}}>{
-  const mList = new Map()
-  for (const [k,v] of jsonMap) {
-    mList.set(`${k}-btns-container`,{vsize: v.length, catnum:{cat: k,num: 0}})
+async function getCategoriesContainers() : Promise<Map<string,{vsize: number,catnum: {cat: string, num: number}}>>{
+  try{
+    const jsonMap = await promiseMap
+    const mList = new Map()
+    for (const [k,v] of jsonMap) {
+      mList.set(`${k}-btns-container`,{vsize: v.length, catnum:{cat: k,num: 0}})
+    }
+    return Promise.resolve(mList)
+  } catch(err){
+    console.log(err)
+    return Promise.resolve(new Map<string,{vsize: number,catnum: {cat: string, num: number}}>)
   }
-  return mList
 }
 
-function getCategoriesNumbersContainers() : Map<string,{cat: string, num: number}> {
-  const mList : Map<string,{cat: string, num: number}> = new Map()
-  for (let h of jsonMap.keys()) {
-    const inList : number[] | undefined = jsonMap.get(h)
-    if(inList){
-      for(let m of inList.keys()){
-        mList.set(`${h}-${m+1}-btns-container`,{cat : h, num: m+1})
+async function getCategoriesNumbersContainers() :Promise<Map<string,{cat: string, num: number}>> {
+  try{
+    const jsonMap = await promiseMap
+    const mList : Map<string,{cat: string, num: number}> = new Map()
+    for (let h of jsonMap.keys()) {
+      const inList : number[] | undefined = jsonMap.get(h)
+      if(inList){
+        for(let m of inList.keys()){
+          mList.set(`${h}-${m+1}-btns-container`,{cat : h, num: m+1})
+        }
       }
     }
+    return mList
+  } catch(err){
+    console.log(err)
+    return Promise.resolve(new Map<string,{cat: string, num: number}>)
   }
-  return mList
 }
 
 function listProblems(uuid : string){
@@ -117,30 +132,35 @@ function listProblems(uuid : string){
 `
 }
 
-function listMachines(uuid : string){
-  document.getElementById(uuid+"-machines")!.innerHTML = `
-      <div class="box-container">
-        <div class="form-container">
-          <form action="/report/add/flow/machines" method="post">
-          <input type="hidden" id="id" name="id" value="${uuid}">
-          <select multiple="multiple" name="machines" id="machines" required>
-            ${
-              (function(){
-                const mList : string[] = []
-                jsonMap.forEach((v,k) =>{
-                  v.forEach(n =>{
-                    mList.push(`<option value="${k}-${n}">${k}${n?'-'+n:''}</option>`)
+async function listMachines(uuid : string){
+  try{
+    const jsonMap = await promiseMap
+    document.getElementById(uuid+"-machines")!.innerHTML = `
+        <div class="box-container">
+          <div class="form-container">
+            <form action="/report/add/flow/machines" method="post">
+            <input type="hidden" id="id" name="id" value="${uuid}">
+            <select multiple="multiple" name="machines" id="machines" required>
+              ${
+                (function(){
+                  const mList : string[] = []
+                  jsonMap.forEach((v,k) =>{
+                    v.forEach(n =>{
+                      mList.push(`<option value="${k}-${n}">${k}${n?'-'+n:''}</option>`)
+                    })
                   })
-                })
-                return mList;
-              })()
-            }
-          </select>
-          <button type="submit">Submit</button>
-          </form>
+                  return mList;
+                })()
+              }
+            </select>
+            <button type="submit">Submit</button>
+            </form>
+          </div>
         </div>
-      </div>
-`
+    `
+  } catch(err){
+    console.log(err)
+  }
 }
 
 function listEmployees(){
@@ -167,39 +187,44 @@ function listEmployees(){
 }
 
 
-function replaceFlowForm(id : string){
-  document.getElementById(id)!.innerHTML =`
-      <div class="box-container">
-        <div class="form-container">
-          <h1> Total Flow record</h1>
-          <form action="/report/flow" method="post">
-          <label name="machines" id="machines">suspended machines</label>
-          <select multiple="multiple" name="machines" id="machines" required>
-            ${
-              (function(){
-                const mList : string[] = []
-                jsonMap.forEach((v,k) =>{
-                  v.forEach(n =>{
-                    mList.push(`<option value="${k}-${n}">${k}${n?'-'+n:''}</option>`)
+async function replaceFlowForm(id : string){
+  try{
+    const jsonMap = await promiseMap
+    document.getElementById(id)!.innerHTML =`
+        <div class="box-container">
+          <div class="form-container">
+            <h1> Total Flow record</h1>
+            <form action="/report/flow" method="post">
+            <label name="machines" id="machines">suspended machines</label>
+            <select multiple="multiple" name="machines" id="machines" required>
+              ${
+                (function(){
+                  const mList : string[] = []
+                  jsonMap.forEach((v,k) =>{
+                    v.forEach(n =>{
+                      mList.push(`<option value="${k}-${n}">${k}${n?'-'+n:''}</option>`)
+                    })
                   })
-                })
-                return mList;
-              })()
-            }
-          </select>
-          <label name="max" id="max">maximum</label>
-          <input type="number" id="max" name="max" required>
-          <label name="min" id="min">minimum</label>
-          <input type="number" id="min" name="min" required>
-          <label name="beginTime" id="beginTime">record begin</label>
-          <input type="time" id="beginTime" name="beginTime" value="${flowMinTime()}" readonly>
-          <label name="endTime" id="endTime">record end</label>
-          <input type="time" id="endTime" name="endTime" min="${flowMinTime()}" max="${shiftEnd()}" required>
-          <button type="submit">Submit</button>
-          </form>
+                  return mList;
+                })()
+              }
+            </select>
+            <label name="max" id="max">maximum</label>
+            <input type="number" id="max" name="max" required>
+            <label name="min" id="min">minimum</label>
+            <input type="number" id="min" name="min" required>
+            <label name="beginTime" id="beginTime">record begin</label>
+            <input type="time" id="beginTime" name="beginTime" value="${flowMinTime()}" readonly>
+            <label name="endTime" id="endTime">record end</label>
+            <input type="time" id="endTime" name="endTime" min="${flowMinTime()}" max="${shiftEnd()}" required>
+            <button type="submit">Submit</button>
+            </form>
+          </div>
         </div>
-      </div>
-`
+    `
+  } catch(err){
+    console.log(err)
+  }
 }
 
 function flowMinTime(){
@@ -226,37 +251,42 @@ function shiftEnd(){
     return newHour+':00'
 }
 
-function replaceTempForm(id: string){
-  document.getElementById(id)!.innerHTML =`
-      <div class="box-container">
-        <div class="form-container">
-          <h1> Temperature record</h1>
-          <form action="/report/temp" method="post">
-          <label name="machine" id="machine">target machine</label>
-          <select name="machine" id="machine" required>
-            ${
-              (function(){
-                const mList : string[] = []
-                jsonMap.forEach((v,k) =>{
-                  if(k == 'KILEN' || k == 'PROJECT'){
-                    v.forEach(n =>{
-                      mList.push(`<option value="${k}-${n}">${k}${n?'-'+n:''}</option>`)
-                    })
-                  }
-                })
-                return mList;
-              })()
-            }
-          </select>
-          <label name="max" id="max">maximum</label>
-          <input type="number" id="max" name="max" required>
-          <label name="min" id="min">minimum</label>
-          <input type="number" id="min" name="min" required>
-          <button type="submit">Submit</button>
-          </form>
+async function replaceTempForm(id: string){
+  try{
+    const jsonMap = await promiseMap
+    document.getElementById(id)!.innerHTML =`
+        <div class="box-container">
+          <div class="form-container">
+            <h1> Temperature record</h1>
+            <form action="/report/temp" method="post">
+            <label name="machine" id="machine">target machine</label>
+            <select name="machine" id="machine" required>
+              ${
+                (function(){
+                  const mList : string[] = []
+                  jsonMap.forEach((v,k) =>{
+                    if(k == 'KILEN' || k == 'PROJECT'){
+                      v.forEach(n =>{
+                        mList.push(`<option value="${k}-${n}">${k}${n?'-'+n:''}</option>`)
+                      })
+                    }
+                  })
+                  return mList;
+                })()
+              }
+            </select>
+            <label name="max" id="max">maximum</label>
+            <input type="number" id="max" name="max" required>
+            <label name="min" id="min">minimum</label>
+            <input type="number" id="min" name="min" required>
+            <button type="submit">Submit</button>
+            </form>
+          </div>
         </div>
-      </div>
-`
+    `
+  } catch(err){
+    console.log(err)
+  }
 }
 
 function replaceNoteForm(id :string){
@@ -286,38 +316,42 @@ function addPlusButtons(element : HTMLElement,catnum : {cat: string, num : numbe
   }
 }
 
-if(true){
-  //show message
-  const message : string | null = new URL(window.location.href).searchParams.get("message")
-  if(message) {
-    document.getElementById("messager")!.innerText = message
-  } else {
-    document.getElementById("messager")!.style.display = "none"
-  }
-//add buttons
-  let btnString = ""
-  for (let catName of jsonMap.keys()) {
-    btnString += `<button class="cat-button"
-                              onclick="replaceButtons('${catName}','add-problem-field')">${catName}</button>`
-  }
-  document.getElementById("add-problem-field")!.innerHTML = btnString
-  // show last flow record delete button
-  const arr = document.getElementsByName('flow-end-time')
-  if(arr.length > 0){
-    document.getElementById(arr[arr.length - 1].id.slice(0,-4) +'-btn')!.style.display = 'block'
+async function messionsOnLaunch(){
+  try{
+    const jsonMap = await promiseMap
+    //show message
+    const message : string | null = new URL(window.location.href).searchParams.get("message")
+    if(message) {
+      document.getElementById("messager")!.innerText = message
+    } else {
+      document.getElementById("messager")!.style.display = "none"
+    }
+  //add buttons
+    let btnString = ""
+    for (let catName of jsonMap.keys()) {
+      btnString += `<button class="cat-button"
+                                onclick="replaceButtons('${catName}','add-problem-field')">${catName}</button>`
+    }
+    document.getElementById("add-problem-field")!.innerHTML = btnString
+    // show last flow record delete button
+    const arr = document.getElementsByName('flow-end-time')
+    if(arr.length > 0){
+      document.getElementById(arr[arr.length - 1].id.slice(0,-4) +'-btn')!.style.display = 'block'
+    }
+
+    const cc = await getCategoriesContainers()
+    for(const [key,value] of cc){
+      if(value.vsize == 1){
+        addPlusButtons(document.getElementById(key)!,value.catnum)
+      }
+    }
+    const cn = await getCategoriesNumbersContainers()
+    for(const [key,value] of cn){
+      addPlusButtons(document.getElementById(key)!,value)
+    }
+  } catch(err){
+    console.log(err)
   }
 }
 
-
-(function (){
-  const cc = getCategoriesContainers()
-  for(const [key,value] of cc){
-    if(value.vsize == 1){
-      addPlusButtons(document.getElementById(key)!,value.catnum)
-    }
-  }
-  const cn = getCategoriesNumbersContainers()
-  for(const [key,value] of cn){
-    addPlusButtons(document.getElementById(key)!,value)
-  }
-})()
+messionsOnLaunch()
